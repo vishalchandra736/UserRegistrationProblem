30c0753999ef889316a6177b07aba2c1dc6d1409
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-20 16:46:54 +0530

    Added : Enter valid first name

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -1,7 +1,17 @@
 package com.blz.userregistrationproblem;
 
+import java.util.Scanner;
+
 public class UserRegistrationProblem {
+    private static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
     public static void main(String[] args) {
+        UserRegistration userRegistration  = new UserRegistration();
         System.out.println("Welcome to User Registration System");
+        System.out.println("Enter the First Name (start with capital and has min 3 character) : " );
+        String firstName = getUserInput();
+        System.out.println(userRegistration.firstName(firstName));
     }
 }
