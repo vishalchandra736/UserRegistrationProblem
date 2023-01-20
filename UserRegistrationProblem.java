e97ffe2d27ecbbe84e153b928f12b6bd9bfb3f84
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-20 16:22:30 +0530

    Added : Welcome to User Registration System

---
+++ UserRegistrationProblem.java
@@ -0,0 +1,7 @@
+package com.blz.userregistrationproblem;
+
+public class UserRegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration System");
+    }
+}
