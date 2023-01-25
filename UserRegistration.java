b7d8f840428dcb7ed569e41074d4e1a7a453abeb
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-25 13:05:54 +0530

    Added : Check Pnone No. using regex

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,10 +1,8 @@
 package com.blz.userregistrationproblem;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class UserRegistration {
-
     public String firstName(String firstName){
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(firstName);
@@ -37,7 +35,6 @@ public class UserRegistration {
         }
         return "Invalid Input!!!";
     }
-    
 }
 
 
