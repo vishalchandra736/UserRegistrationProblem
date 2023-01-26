56432261ea49611c37ee1e031d4b5c1a8ab88016
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-26 23:42:28 +0530

    Added : Check password using regex

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,37 +3,38 @@ package com.blz.userregistrationproblem;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class UserRegistration {
-    public String firstName(String firstName){
+    public void checkFirstName(String firstName){
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(firstName);
-        if(matcher.matches()) {
-            return "Valid Input";
-        }
-        return "Invalid Input!!!";
+        match(matcher.matches());
     }
-    public String lastName(String lastName){
+    public void checkLastName(String lastName){
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(lastName);
-        if(matcher.matches()) {
-            return "Valid Input";
-        }
-        return "Invalid Input!!!";
+        match(matcher.matches());
     }
-    public String email(String email){
+    public void checkEmail(String email){
         Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([.-_+]*[a-zA-Z0-9]*)[@][a-z]+[.][a-zA-Z]{2,4}+([.]*[a-zA-Z]{2,4})?$");
         Matcher matcher = pattern.matcher(email);
-        if(matcher.matches()) {
-            return "Valid Input";
-        }
-        return "Invalid Input!!!";
+        match(matcher.matches());
     }
-    public String checkPhoneNo(String phoneNo){
+    public void checkPhoneNo(String phoneNo){
         Pattern pattern = Pattern.compile("^[0-9]{1,3}[\\s]{1}[0-9]{10}$");
         Matcher matcher = pattern.matcher(phoneNo);
-        if(matcher.matches()) {
-            return "Valid Input";
+        match(matcher.matches());
+    }
+    public void checkPassword(String password){
+        Pattern pattern = Pattern.compile("^[A-Za-z0-9]{8,}$");
+        Matcher matcher = pattern.matcher(password);
+        match(matcher.matches());
+    }
+    public static void match(boolean match) {
+        if(match) {
+            System.out.println("Valid Input");
         }
-        return "Invalid Input!!!";
+        else {
+            System.out.println("InValid Input!!!");
+        }
     }
 }
 
