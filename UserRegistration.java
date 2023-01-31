c53a34852127f6f40eb9cb7af5fa14ed8140b912
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-01 00:43:31 +0530

    Added : check all the email samples

--- UserRegistration.java
+++ UserRegistration.java
@@ -36,6 +36,31 @@ public class UserRegistration {
             System.out.println("InValid Input!!!");
         }
     }
+
+    public void testMails() {
+        checkEmail("abc@yahoo.com");
+        checkEmail("abc");
+        checkEmail("abc-100@yahoo.com");
+        checkEmail("abc@.com.my");
+        checkEmail("abc.100@yahoo.com");
+        checkEmail("abc123@gmail.a");
+        checkEmail("abc111@abc.com");
+        checkEmail("abc123@.com");
+        checkEmail("abc-100@abc.net");
+        checkEmail("abc123@.com.com");
+        checkEmail("abc.100@abc.com.au");
+        checkEmail(".abc@abc.com");
+        checkEmail("abc@1.com");
+        checkEmail("abc()*@gmail.com");
+        checkEmail("abc@gmail.com.com");
+        checkEmail("abc@%*.com");
+        checkEmail("abc+100@gmail.com");
+        checkEmail("abc..2002@gmail.com");
+        checkEmail("abc.@gmail.com");
+        checkEmail("abc@abc@gmail.com");
+        checkEmail("abc@gmail.com.1a");
+        checkEmail("abc@gmail.com.aa.au");
+    }
 }
 
 
