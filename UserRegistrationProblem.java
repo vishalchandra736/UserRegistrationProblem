5020f1a63e43250e5f463695e249d3e05eecd417
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-01 00:35:36 +0530

    Added : In password, required atleast 1 special character, 1 number and 1 uppercase letter

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -21,7 +21,7 @@ public class UserRegistrationProblem {
         System.out.println("Enter the Phone No (country code follow by space and 10 digit number): " );
         String phoneNo = getUserInput();
         userRegistration.checkPhoneNo(phoneNo);
-        System.out.println("Enter the Password (min 8 char and atleast 1 uppercase letter, and 1 number) : " );
+        System.out.println("Enter the Password (min 8 character and atleast 1 special character, 1 uppercase letter, and 1 number) : " );
         String password = getUserInput();
         userRegistration.checkPassword(password);
     }
